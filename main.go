e9873ec107c66715e39cfc10a9bf0a6de1ba3cd9
LAVANYA J C <139207138+Lavanyajc@users.noreply.github.com>
2025-06-28 10:04:17 +0530

    Update main.go

--- main.go
+++ main.go
@@ -38,6 +38,18 @@ func writeCounter(counter Counter) {
 
 func main() {
 	r := gin.Default()
+	 r.Use(func(c *gin.Context) {
+	       c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	       c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	       c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
+	       if c.Request.Method == "OPTIONS" {
+		    c.AbortWithStatus(204)
+	            return
+	       }
+	       c.Next()
+       })
+
+
 
 	r.GET("/visits", func(c *gin.Context) {
 		mu.Lock()
